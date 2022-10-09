563d63479b7dd6845b514c2d69264173d579d109
Rasmus Wiktell Sundman <rsundman04@gmail.com>
2022-10-09 18:18:24 +0200

    Moved ReturnJson middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
+        ReturnJson::class,
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
@@ -32,7 +33,6 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            ReturnJson::class
         ],
 
         'api' => [
