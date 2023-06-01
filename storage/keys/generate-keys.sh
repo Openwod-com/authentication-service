680bfacecdc6bd123daf734180274d1e5040fa1d
Rasmus Wiktell Sundman <rsundman04@gmail.com>
2023-06-01 10:36:29 +0200

    Update generate-keys.sh

--- storage/keys/generate-keys.sh
+++ storage/keys/generate-keys.sh
@@ -1,3 +1,3 @@
-sudo ssh-keygen -m pem -f ./sign_jwt -q -N ""
-sudo ssh-keygen -y -f sign_jwt -m pem -e > sign_jwt.pub
+sudo ssh-keygen -m pem -f sign_jwt -q -N ""
+openssl rsa -in sign_jwt -pubout -out sign_jwt.pub
 sudo chown www-data:www-data *
